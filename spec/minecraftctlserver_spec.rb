9cfa54e42b0d6ee82b4ab54c5f4f78ba73854807
Jakub Pastuszek <jpastuszek@gmail.com>
2011-09-26 22:31:22 +0200

    pid file GET and better server shutdown detection in tests

--- spec/minecraftctlserver_spec.rb
+++ spec/minecraftctlserver_spec.rb
@@ -5,18 +5,16 @@ require 'spawn'
 
 $url = 'http://localhost:25560/'
 
-def start_stub(wait = true)
+def start_stub
 	pid, stdin, stdout, stderr = Spawn::spawn(File.dirname(__FILE__) + '/../bin/minecraftctlserver -c ./minecraft ' + File.dirname(__FILE__) + '/stub_server')
 
-	if wait
-		c = HTTPClient.new
-		Timeout.timeout(10) do
-			begin
-				c.get_content($url + "server/status")
-			rescue Errno::ECONNREFUSED
-				sleep 0.4
-				retry
-			end
+	c = HTTPClient.new
+	Timeout.timeout(10) do
+		begin
+			@pid_file = c.get_content($url + "pid_file").strip
+		rescue Errno::ECONNREFUSED
+			sleep 0.1
+			retry
 		end
 	end
 end
@@ -24,67 +22,41 @@ end
 def stop_stub
 	HTTPClient.new.post_content($url, 'shutdown')
 
-	Timeout.timeout(10) do
-		begin
-			loop do
-				HTTPClient.new.get_content($url + "server/status")
-				sleep 0.4
-			end
-		rescue Errno::ECONNREFUSED
-		rescue => e
-			puts "got different error: #{e}"
-		end
+	# wait pid lock release
+	File.open(@pid_file) do |pf|
+		pf.flock(File::LOCK_EX)
 	end
-	sleep 0.2
 end
 
 describe 'minecraftctlserver' do
-	describe 'text mode HTTP' do
-		describe  'server startup' do
-			before :all do
-				start_stub(false)
-			end
-
-			it 'should start the minecraft server and respond to status command when ready' do
-				Timeout.timeout(10) do
-					out = nil
-					begin
-						out = HTTPClient.new.get_content($url + "server/status")
-					rescue Errno::ECONNREFUSED
-						sleep 0.4
-						retry
-					end
-
-					out.should =~ /running/
-				end
-			end
-
-			after :all do
-				stop_stub
-			end
+	describe 'while server ready it should respond to' do
+		before :all do
+			start_stub
 		end
 
-		describe 'while server ready' do
-			before :all do
-				start_stub
-			end
+		it 'GET /pid_file with absolute pid file path' do
+			HTTPClient.new.get_content($url + 'pid_file').should match(%r{/.*spec/stub_server/minecraftctlserver.pid})
+		end
 
-			it 'should respond to console list command' do
-				HTTPClient.new.post_content($url + 'server', 'console list').should == "Connected players: kazuya\n"
-			end
+		it 'GET / with API command list' do
+			HTTPClient.new.get_content($url).should include 'minecraft control server API'
+		end
 
-			it 'should stop and start with POST /server start and POST /server stop' do
-				HTTPClient.new.post_content($url + 'server', 'stop').should include "Server stopped\n"
-				HTTPClient.new.post_content($url + 'server', 'start').should include "Done (5887241893ns)! For help, type \"help\" or \"?\"\n"
-			end
+		it 'GET /pid with PID number of control server process' do
+			HTTPClient.new.get_content($url + 'pid').to_i.should > 0
+		end
 
-			it 'should respond to GET / with API command list' do
-				HTTPClient.new.get_content($url).should include 'minecraft control server API'
-			end
+		it 'POST /server/console list command' do
+			HTTPClient.new.post_content($url + 'server', 'console list').should == "Connected players: kazuya\n"
+		end
 
-			after :all do
-				stop_stub
-			end
+		it 'stop and start with POST /server stop and POST /server start' do
+			HTTPClient.new.post_content($url + 'server', 'stop').should include "Server stopped\n"
+			HTTPClient.new.post_content($url + 'server', 'start').should include "Done (5887241893ns)! For help, type \"help\" or \"?\"\n"
+		end
+
+		after :all do
+			stop_stub
 		end
 	end
 end
